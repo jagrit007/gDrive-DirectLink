3d521415028c2f7c64b1b39df95d40cf4cf7d319
jagrit007 <40325232+jagrit007@users.noreply.github.com>
2019-06-26 20:11:45 +0530

    sorry but I wasn't feeling good

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ Don't Forget to rename file after Download with extension!!
 
                                 Thanks!""")
 
-api = "PasteHere"
-
-#Dont Edit ANYTHING Below THIS
+api = "AIzaSyCRtAXecCXzA-kNIKtJWWO85R2e4v8FEDA"
 file_id=""
 i = ""
 list1 = []
@@ -20,8 +18,11 @@ while i != "q":
 
 list1.pop(-1)
 #print(list1)
+#https://drive.google.com/open?id=1smGR_N5Uxs2-FFHmd_niD699mi56dx3W
+#https://drive.google.com/a/students.solano.edu/file/d/127h9cSCEuzOtiNeUrtQKkJx0-UcIuETb/view?usp=drivesdk
+#https://drive.google.com/uc?id=1n_vtbTusz_JYEQfdYXaR-Ai3-xwNEjs9&export=download
 for link in list1:
-  if link.find("file/d/") != -1:
+    if link.find("file/d/") != -1:
         id_part = link.find("file/d/")
         id = link[id_part+7:]
         new_id = id.replace("/view?usp=drivesdk", "")
