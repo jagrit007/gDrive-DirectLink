2d2a2a134d109c0d05a137a4630ea3ddf5fa3cf0
jagrit007 <40325232+jagrit007@users.noreply.github.com>
2019-06-26 19:20:19 +0530

    Updated to v2
    Better ways of extracting URLs, more reliable.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+# Credits : @jagrit007 (GitHub)
+# https://github.com/jagrit007
+print("""Welcome to the Direct Link Generator by @jagrit007
+for any issues report to me on Telegram : @notSbeve
+Paste as much shareable links as you wish and when you're done just enter q
+Make sure you've entered your own API key in main.py
+Don't Forget to rename file after Download with extension!!
+
+                                Thanks!""")
+
 api = "PasteHere"
+
+#Dont Edit ANYTHING Below THIS
 file_id=""
 i = ""
 list1 = []
@@ -9,10 +21,18 @@ while i != "q":
 list1.pop(-1)
 #print(list1)
 for link in list1:
-    id_part = link.find("id=")
-    id = link[id_part+3:]
-    #print(id)
-    final_url = f"https://www.googleapis.com/drive/v3/files/{id}?alt=media&key={api}"
-    print(f"""
-    {final_url}
-    """)
+    if link.find("&export=download") == -1:
+        id_part = link.find("id=")
+        id = link[id_part+3:]
+        #print(id)
+        final_url = f"https://www.googleapis.com/drive/v3/files/{id}?alt=media&key={api}"
+        print(f"""
+        {final_url}
+        """)
+    else:
+        id_part = link.find("id=")
+        id = link[id_part+3:-16]
+        final_url = f"https://www.googleapis.com/drive/v3/files/{id}?alt=media&key={api}"
+        print(f"""
+        {final_url}
+        """)
